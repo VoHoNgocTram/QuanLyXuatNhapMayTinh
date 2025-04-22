744a6d9d43500207b8932b900199692f734267ff
VoHoNgocTram <114790013+VoHoNgocTram@users.noreply.github.com>
2025-04-22 13:30:35 +0700

    update Tìm Kiếm Đơn nghỉ

--- src/DAO/DonNghiDAO.java
+++ src/DAO/DonNghiDAO.java
@@ -5,13 +5,12 @@
 package DAO;
 
 import DTO.DonNghiDTO;
-import DTO.NhaCungCapDTO;
 import database.JDBCUtil;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Date;
-
+import java.sql.SQLException;
+import java.sql.Date;
 /**
  *
  * @author downny
@@ -48,6 +47,36 @@ public class DonNghiDAO {
         }
         return ketQua;
     }
+    
+    
+    public ArrayList<DonNghiDTO> danhSachDonNghiTheoTrangThai (int trangThai){
+        ArrayList<DonNghiDTO> dsDonNghi = new ArrayList();
+        try {
+            java.sql.Connection con = JDBCUtil.getConnection();
+            String sql = "SELECT * FROM donnghi WHERE trangthai = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, trangThai);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {                
+                int maDonNghi = rs.getInt("madn");
+                String hoTen = rs.getString("hoten");
+                String loaiNghi = rs.getString("loai_nghi");
+                Date ngayNopDon = rs.getDate("ngaynopdon");
+                Date ngayBatDau = rs.getDate("ngaybatdau");
+                Date ngayKetThuc = rs.getDate("ngayketthuc");
+                String ghiChu = rs.getString("ghichu");
+                int status = rs.getInt("trangthai");
+                
+                DonNghiDTO donNghi = new DonNghiDTO(maDonNghi, hoTen, loaiNghi, ngayNopDon, ngayBatDau, ngayKetThuc, ghiChu, trangThai);
+                dsDonNghi.add(donNghi);
+            }
+            JDBCUtil.closeConnection(con);
+         } catch (SQLException e) {
+                e.printStackTrace();
+        }
+        return dsDonNghi;  
+    }
+    
     
     
 }
